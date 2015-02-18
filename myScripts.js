2b7e94b430fbb921b6fa80f46324fa23ad2ebc9e
githubsven <s.luehof@gmail.com>
2015-02-18 16:47:49 +0100

    WOOP WOOP

--- myScripts.js
+++ myScripts.js
@@ -1,57 +1,21 @@
-var ids = new Array();
-var titles = new Array();
-
-function showGallery(){
-	var sel = document.getElementById("Galleries");
-	var selectedID = sel.options[sel.selectedIndex].value;
-	$(".largeImageWrap").each(function(i){
-		$(this).remove()
-	});
-	$(".sliderGallery_Wrap").each(function(i){
-		$(this).remove()
-	});
-	$('#flickrTest').flickrGallery({
-		galleryHeight : 'auto',
-		useFlickr: 'true',
-		useFlickrLargeSize: 'true',
-		useHoverIntent: 'true',
-		flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
-		galleryID: selectedID,
-		useLightBox: 'true',
-		per_page: 50
-	});
-}
-
 $(document).ready(function(){
 	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.galleries.getPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&gallery_id=129503413-72157648443716594&format=json&jsoncallback=?",
 	function(data){
-		var galleryID = "";
-		var title = "";
+		var id = "";
+		var farm = "";
+		var secret = "";
+		var server = "";
+		var images = "";
 		
 		$.each(data.photos.photo, function(i,set){
-			galleryID = set.id;
-			title = set.title;
-			ids.push(galleryID);
-			titles.push(title);
+			id = set.id;
+			farm = set.farm;
+			secret = set.secret;
+			server = set.server;
+			images += "<img src = 'https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_q" + ".jpg'/>"
 		});
 		
-		for(i=0; i<ids.length;i++){
-			var option = new Option(titles[i],ids[i]);
-			var dropDown = document.getElementById("Galleries");
-			dropDown.options[i] = option;
-		}
-
-		$('#flickrTest').flickrGallery({
-			galleryHeight : 'auto',
-			useFlickr: 'true',
-			useFlickrLargeSize: 'true',
-			useHoverIntent: 'true',
-			flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
-			galleryID: ids[0],
-			useLightBox: 'true',
-			per_page: 50
-		});
-	
+		$("#flickrTest").html(images);
 	});
 	
 	$("nav a").hover(function(){
