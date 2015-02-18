ea59f18a2c3f1fb66df8bc32576c6ea4dbeb435f
githubsven <s.luehof@gmail.com>
2015-02-18 15:33:07 +0100

    YAAY HET WERKT! :dancer:

--- myScripts.js
+++ myScripts.js
@@ -1,43 +1,34 @@
-/*
-$(document).ready(function(){
-	$.get("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos", { api_key: "787af8ce3db07ed8b23566aa9c6a416e", user_id: "29096781@N02" }, function(data){
-		alert(data);
-	});
-});
-*/
-<script type="text/javascript">
-
 var ids = new Array();
 var titles = new Array();
 
 function showGallery(){
-var sel = document.getElementById("Galleries");
-var selectedID = sel.options[sel.selectedIndex].value;
-$(".largeImageWrap").each(function(i){
-$(this).remove()
-});
-$(".sliderGallery_Wrap").each(function(i){
-$(this).remove()
-});
-$('#flickrTest').flickrGallery({
-galleryHeight : 'auto',
-useFlickr: 'true',
-useFlickrLargeSize: 'true',
-useHoverIntent: 'true',
-flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
-galleryID: selectedID,
-useLightBox: 'true',
-per_page: 50
-});
+	var sel = document.getElementById("Galleries");
+	var selectedID = sel.options[sel.selectedIndex].value;
+	$(".largeImageWrap").each(function(i){
+		$(this).remove()
+	});
+	$(".sliderGallery_Wrap").each(function(i){
+		$(this).remove()
+	});
+	$('#flickrTest').flickrGallery({
+		galleryHeight : 'auto',
+		useFlickr: 'true',
+		useFlickrLargeSize: 'true',
+		useHoverIntent: 'true',
+		flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
+		galleryID: selectedID,
+		useLightBox: 'true',
+		per_page: 50
+	});
 }
 
-$(document).ready(Function(){
-	$getJSON("https://api.flickr.com/services/rest/?&method=flickr.galleries.getPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&gallery_id=129503413-72157648443716594&format=json&jsoncallback=?",
+$(document).ready(function(){
+	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.galleries.getPhotos&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&gallery_id=129503413-72157648443716594&format=json&jsoncallback=?",
 	function(data){
 		var galleryID = "";
 		var title = "";
+		
 		$.each(data.photos.photo, function(i,set){
-			
 			galleryID = set.id;
 			title = set.title;
 			ids.push(galleryID);
@@ -45,23 +36,28 @@ $(document).ready(Function(){
 		});
 		
 		for(i=0; i<ids.length;i++){
-		var option = new Option(titles[i],ids[i]);
-		var dropDown = document.getElementById("Galleries");
-		dropDown.options[i] = option;
+			var option = new Option(titles[i],ids[i]);
+			var dropDown = document.getElementById("Galleries");
+			dropDown.options[i] = option;
 		}
 
 		$('#flickrTest').flickrGallery({
-		galleryHeight : 'auto',
-		useFlickr: 'true',
-		useFlickrLargeSize: 'true',
-		useHoverIntent: 'true',
-		flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
-		galleryID: ids[0],
-		useLightBox: 'true',
-		per_page: 50
+			galleryHeight : 'auto',
+			useFlickr: 'true',
+			useFlickrLargeSize: 'true',
+			useHoverIntent: 'true',
+			flickrAPIKey: '4ef2fe2affcdd6e13218f5ddd0e2500d',
+			galleryID: ids[0],
+			useLightBox: 'true',
+			per_page: 50
 		});
 	
 	});
 	
+	$("nav a").hover(function(){
+		$(this).stop().animate({color: "green"}, 300);
+	}, function(){
+		$(this).stop().animate({color: "white"}, 300);
+	});
+	
 });
-</script>
