281bdb611980c5ef4407752d2c404ad8a72e1fbe
githubsven <s.luehof@gmail.com>
2015-02-20 13:54:24 +0100

    PING 2.0

--- myScripts.js
+++ myScripts.js
@@ -3,17 +3,28 @@ $(document).ready(function(){
 		getPhotos();
 		
 		$("nav a").hover(function(){
-			$(this).stop().animate({color: "green"}, 300);
+			$(this).stop().animate({color: "#545454"}, 300);
 		}, function(){
-			$(this).stop().animate({color: "white"}, 300);
+			$(this).stop().animate({color: "#ffffff"}, 300);
 		});
 		
+		$("#1").hover(function(){
+			$(this).stop().animate({'width' : '100px', 'height' : '100px', 'padding' : '15px'}, 300);
+		}, function(){
+			$(this).stop().animate({'height' : '50px', 'width' : '50px', 'padding' : '5px'}, 300);
+		});
+		
+		$("#searchValue").keypress(function(e){
+			if(e.keyCode == 13)
+				$("#searchBtn").click();
+		});
+
 });
 
 function getPhotos(){
-	var text_value = $("#text").val();	
-	var aantal = 104;
-	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&text=" + text_value + "&per_page="+ aantal + " &format=json&jsoncallback=?",
+	var searchBtn = $("#searchValue").val();	
+	var aantal = 54;
+	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&text=" + searchBtn + "&per_page="+ aantal + " &format=json&jsoncallback=?",
 		function(data){
 			var id = "";
 			var farm = "";
@@ -26,14 +37,25 @@ function getPhotos(){
 				farm = set.farm;
 				secret = set.secret;
 				server = set.server;
-				images += "<img src = 'https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_q" + ".jpg'/>"
+				images += "<img class = 'flickrImg' id = '" + i + "' onclick = 'enlargePhoto(" + i + ")' src = 'https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_q" + ".jpg'/>"
 			});
 			
-			$("#flickrTest").html(images);
+			$("#flickrResult").html(images);
 		});
 }
 
-function emphasizePhoto(){
-	$("#overlay").css("display", "block");
+function enlargePhoto(id){
+	$(".overlay").css("display", "block");
+	var src = $("#" + id).attr("src");
+	var enhancedSrc = src.substring(0, src.length - 5) + "c.jpg";
+	var image = "<img src = '" + enhancedSrc + "'/>";
+	$("#bigPhotoSection").html(image);
+	$("#bigPhotoSection").css("display", "block");
+}
+
+function hidePhotoSection(){
+	$(".overlay").css("display", "none");
+	$("#bigPhotoSection").html("");
+	$("#bigPhotoSection").css("display", "none");
 }
 	
