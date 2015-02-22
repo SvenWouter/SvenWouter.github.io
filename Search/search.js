39d2537d43fc92958f2de2d952b5ea94eb62c6c0
githubsven <s.luehof@gmail.com>
2015-02-22 23:09:49 +0100

    Fucking Grote Layout Update...

--- Search/search.js
+++ Search/search.js
@@ -6,29 +6,47 @@ $(document).ready(function(){
 			if(e.keyCode == 13)
 				getPhotos();
 		});
+		
+		$("#resultCount").keypress(function(e){
+			if(e.keyCode == 13)
+				getPhotos();
+		});
+		
+		$("#searchBtn").hover(function(){
+			$(this).stop().animate({backgroundColor: "#003366"}, 300);
+		}, function(){
+			$(this).stop().animate({backgroundColor: "#336699"}, 300);
+		});
 
 });
 
 function getPhotos(){
-	var searchBtn = $("#searchValue").val();	
-	var aantal = 60;
-	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&text=" + searchBtn + "&per_page="+ aantal + " &format=json&jsoncallback=?",
+	var searchQuery = $("#searchValue").val();	
+	var aantal = $("#resultCount").val();
+	var rowSize = Math.ceil(aantal / 3);
+	var count = -1;
+	$.getJSON("https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=4ef2fe2affcdd6e13218f5ddd0e2500d&text=" + searchQuery + "&per_page="+ aantal + " &format=json&jsoncallback=?",
 		function(data){
 			var id = "";
 			var farm = "";
 			var secret = "";
 			var server = "";
-			var images = "";
-			
+			var images = "<tr>";
+	
 			$.each(data.photos.photo, function(i,set){
+				count++;
 				id = set.id;
 				farm = set.farm;
 				secret = set.secret;
 				server = set.server;
-				images += "<img class = 'flickrImg' id = '" + i + "' onclick = 'enlargePhoto(" + i + ")' src = 'https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_q" + ".jpg'/>"
+				if(count == rowSize){
+					count = 0;
+					images += "</tr><tr>";
+				} 
+				images += "<td><img class = 'flickrImg' id = '" + i + "' onclick = 'enlargePhoto(" + i + ")' src = 'https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_q" + ".jpg'/></td>"
 			});
-			
-			$("#flickrResult").html(images);
+			images += "</tr>";
+			$("#flickrTable").html(images);
 		});
 }
 
