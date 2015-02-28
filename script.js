81131d649c0b217207a343f7dd6875408e639e04
githubsven <s.luehof@gmail.com>
2015-02-28 17:38:12 +0100

    Super opmaak hippe shit braaaah :wolf:
    WOOP WOOP PING PING

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 
 	$("nav a").hover(function(){
-			$(this).stop().animate({color: "#FFA812"}, 300);
+			$(this).stop().animate({color: "#E74C3C"}, 300);
 		}, function(){
 			$(this).stop().animate({color: "#FFFFFF"}, 300);
 	});
 	
-	$.each($(".source"), function(){
-		
+	$(".source").each(function(){
+		var source = $(this).attr("href");
+		var html = "<p>" + "<a href = '" + source + "'>" + source + "</a>" + "</p>";
+		$("footer").append(html);
 	});
 	
 });
