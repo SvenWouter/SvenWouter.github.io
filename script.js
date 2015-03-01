7ee87a437bbf174c80c7c4f4028b8d48e3f10a60
githubsven <s.luehof@gmail.com>
2015-03-01 18:15:13 +0100

    Opmaak en Scroll fix

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
+	var sourceCounter = 0;
+	$(".source").each(function(){
+		sourceCounter++;
+		$(this).attr("id", sourceCounter);
+		var id = $(this).attr("id");
+		var source = $(this).attr("href")
+		var html = "<p>" + "<a href = '#" + id + "'>" + source + "</a>" + "</p>";
+		$("footer").append(html);
+	});
+
 	$("a").hover(function(){
 		$(this).data("original", $(this).css("color"));
 		$(this).stop().animate({color: "#E74C3C"}, 300);
 	}, function(){
 		$(this).stop().animate({color: $(this).data("original")}, 300);
-	});
-	
-	$(".source").each(function(){
-		var source = $(this).attr("href");
-		var html = "<p>" + "<a href = '" + source + "'>" + source + "</a>" + "</p>";
-		$("footer").append(html);
 	});
 	
 });
